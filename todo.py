37b2624f101e3daf88503da1dbfbe8a1546fb9aa
Manuel Aigner <manuelaigner23@googlemail.com>
2020-09-28 21:20:51 +0200

    Added functions for parsers and removed handlers

--- todo.py
+++ todo.py
@@ -9,109 +9,105 @@ import os
 import sys
 import argparse
 import json
-
-from datetime import date
+import task_db
 
 META_FOLDER = 'C:/scripts/todo'
-DATA = "C:/data/todo/database.json"
+DATA = "D:/data/todo/database.json"
 TASK_SECTION = 'tasks'
 
-def show_list():
-    database = load_todo_list_from_storage()
-    for todo in database[TASK_SECTION]:
-        print('[' + str(todo['id']) + ']: ' + todo['date'] + ' : ' + todo['title'])
+taskDBHandle = task_db.TaskDBHandler(DATA)
 
-def add_task(arg):
-    database = load_todo_list_from_storage()
-    database[TASK_SECTION].append({
-        'id': generate_id(database),
-        'title': arg[0],
-        'date': str(date.today().strftime("%d-%m-%Y"))
-    })
-    store_todo_list(database)
+#def show_list():
+#    database = load_todo_list_from_storage()
+#    for todo in database[TASK_SECTION]:
+#        print('[' + str(todo['id']) + ']: ' + todo['date'] + ' : ' + todo['title'])#
 
-def generate_id(database):
-    if not database[TASK_SECTION]:
-        return 0
+#def generate_id(database):
+#    if not database[TASK_SECTION]:
+#        return 0
+#    else:
+#        return int(database[TASK_SECTION][-1]['id']) + 1
+
+#def remove_task(id):
+#    database = load_todo_list_from_storage()
+#    
+#    for task in database[TASK_SECTION]:
+#        if (task['id']) == id:
+#            database[TASK_SECTION].remove(task)
+#            store_todo_list(database)
+#            return
+#    
+#    print('No task found with id: ' + str(id))
+#    show_list()
+
+def command_add_task(args):
+
+    task = task_db.Task()
+
+    if args.task:
+        task.title = args.task[0]
     else:
-        return int(database[TASK_SECTION][-1]['id']) + 1
+        raise ValueError('When adding a task a title must be set. Use -t [TASK_TITLE] to specify it.')
 
-def remove_task(id):
-    database = load_todo_list_from_storage()
+    if args.description:
+        task.description = args.description[0]
+
+    if args.priority:
+        task.priority = args.priority[0]
+
+    taskDBHandle.add_task(task)
+
+def command_list_task(args):
     
-    for task in database[TASK_SECTION]:
-        if (task['id']) == id:
-            database[TASK_SECTION].remove(task)
-            store_todo_list(database)
-            return
-    
-    print('No task found with id: ' + str(id))
-    show_list()
+    if args.all:
+        for task in taskDBHandle.tasks:
+            print(task)
+            
+def command_edit(args):
+    print('list all tasks')
 
-def command_add(args):
-    print('add parser activated')
-
-def command_list(args):
-    print('list parser activated')
 
 def command_line():
     parser = argparse.ArgumentParser(prog='PROG', description='This is a command line todo-list.')
     parser.add_mutually_exclusive_group()
-    parser.add_argument('-l', '--list', action='store_true')
-    parser.add_argument('-a', '--add', action='store', nargs=1)
-    parser.add_argument('-r', '--remove', action="store", nargs=1, type=int)
-    parser.add_argument('-d', '--description', action='store', nargs=1)
-    parser.add_argument('-p', '--priority', action="store", nargs=1)
     subparsers = parser.add_subparsers()
-    
-    
+
+    #create parser for adding tasks
     add_parser = subparsers.add_parser('add', help='add new task')
-    add_parser.set_defaults(func=command_add)
+    add_parser.set_defaults(func=command_add_task)
     add_parser.add_argument('-t', '--task', action='store', nargs=1, help="Adds an task.")
     add_parser.add_argument('-d', '--description', action='store', nargs=1, help="Adds an description to an task")
     add_parser.add_argument('-p', '--priority', action='store', nargs=1, help='adds a priority to a task')
-
+    
+    #create parser for listing tasks
     list_parser = subparsers.add_parser('list', help='list tasks and settings')
-    list_parser.set_defaults(func=command_list)
-    list_parser.add_mutually_exclusive_group()
+    list_parser.set_defaults(func=command_list_task)
     list_parser.add_argument('-a', '--all', action='store_true', help='lists all stored task in random order')
-    list_parser.add_argument('-t', '--task', action='store', nargs=1, help='list task with specified id')
+    list_parser.add_argument('-i', '--id', action='store', nargs=1, help='list task with specified id')
     list_parser.add_argument('-c', '--category', action='store', nargs=1, help='list all tasks with specified category')
     list_parser.add_argument('-p', '--priority', action='store', nargs=1, help='list all tasks with priority greater then or equal to specified number')
     
+    #create parser for editing tasks
+    edit_parser = subparsers.add_parser('edit', help='edit tasks and settings')
+    edit_parser.set_defaults(func=command_edit)
+    edit_parser.add_argument('-i', '--task', action='store', nargs=1, help='list task with specified id')
+    edit_parser.add_argument('-t', '--title', action='store', nargs=1, help='list task with specified id')
     
+
+    #create parser for removing tasks
+
+    #create parser for workspace creator
+
     try:
         args = parser.parse_args()
     except SystemExit as e:
         if e.code != 0:
             print("Invalid arguments")
             sys.exit(e.code)
-
-    args.func(args)
-
-    if args.list:
-        show_list()
-    elif args.add:
-        add_task(args.add)
-    elif args.remove:
-        remove_task(args.remove[0])
-    elif args.task:
-        print(args.task[0])
-    if args.description:
-        print(args.description[0])
-
-def load_todo_list_from_storage():
-    database = {}
-    database[TASK_SECTION] = []
-    
-    if os.path.isfile(DATA):
-        with open(DATA) as json_file:
-            database = json.load(json_file)
-    return database
-
-def store_todo_list(database):
-    with open(DATA, 'w') as json_file:
-        json.dump(database, json_file, indent=2)
+    try:
+        args.func(args)
+    except ValueError as e:
+        print(e)
 
 def main():
     command_line()
